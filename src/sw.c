d5ee710e80daba8efb395a4cb40e939e2406c9fd
Patrick Ziller <patrickandstuff@gmail.com>
2023-03-24 11:30:33 -0400

    press space to pause, q to quit

--- src/sw.c
+++ src/sw.c
@@ -6,6 +6,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <getopt.h>
+#include <pthread.h>
+#include <termios.h>
 #include <sys/stat.h>
 
 #define NSEC_TO_SLEEP 10000000
@@ -23,28 +25,43 @@ struct timespec starttime;
 struct timespec currenttime;
 struct timespec elapsedtime;
 struct timespec restored_time;
+struct timespec resumedtime;
+struct timespec pausedtime;
 
 char endchar = '\r';
 
 int rflag = 0;
 int sflag = 0;
 
+void set_canonical_mode(int enable)
+{
+  struct termios term_settings;
+  tcgetattr(STDIN_FILENO, &term_settings);
+
+  if (enable)
+    term_settings.c_lflag |= ICANON | ECHO;
+  else
+    term_settings.c_lflag &= ~(ICANON | ECHO);
+
+  tcsetattr(STDIN_FILENO, TCSANOW, &term_settings);
+}
+
 void print_time(FILE *fd)
 {
   if (elapsedtime.tv_sec >= 3600)
   {
     fprintf(fd, OVER_HOUR_TEMPLATE,
-            (int)(elapsedtime.tv_sec / 3600),
-            (int)(elapsedtime.tv_sec % 3600 / 60),
-            (int)(elapsedtime.tv_sec % 60),
-            (int)(elapsedtime.tv_nsec / 10000000));
+        (int)(elapsedtime.tv_sec / 3600),
+        (int)(elapsedtime.tv_sec % 3600 / 60),
+        (int)(elapsedtime.tv_sec % 60),
+        (int)(elapsedtime.tv_nsec / 10000000));
   }
   else
   {
     fprintf(fd, UNDER_HOUR_TEMPLATE,
-            (int)(elapsedtime.tv_sec / 60),
-            (int)(elapsedtime.tv_sec % 60),
-            (int)(elapsedtime.tv_nsec / 10000000));
+        (int)(elapsedtime.tv_sec / 60),
+        (int)(elapsedtime.tv_sec % 60),
+        (int)(elapsedtime.tv_nsec / 10000000));
   }
   fprintf(fd, "%c", endchar);
   fflush(stdout);
@@ -136,46 +153,91 @@ void restore_time()
   }
 }
 
-void sigint_handler(int sig)
-{
+void cleanup() {
   timing = 0;
   endchar = '\n';
   clear_output();
   print_time(stdout);
   SHOW_CURSOR;
+  set_canonical_mode(1);
   if (sflag)
   {
     save_time();
   }
+}
+void sigint_handler(int sig)
+{
   signal(sig, sigint_handler);
+}
+
+void* input_thread(void* arg)
+{
+  char c;
+  set_canonical_mode(0);
+  while ((c = getchar()) != EOF)
+  {
+    switch (c) {
+      case ' ':
+        // pause or resume stopwatch
+        timing = !timing;
+        if (!timing) {
+          clock_gettime(CLOCK_MONOTONIC, &pausedtime);
+        }
+        if (timing) {
+          clock_gettime(CLOCK_MONOTONIC, &resumedtime);
+          starttime.tv_sec += resumedtime.tv_sec - pausedtime.tv_sec;
+          starttime.tv_nsec += resumedtime.tv_nsec - pausedtime.tv_nsec;
+          if (starttime.tv_nsec < 0)
+          {
+            starttime.tv_nsec += 1000000000;
+            --starttime.tv_sec;
+          } else if (starttime.tv_nsec >= 1000000000)
+          {
+            starttime.tv_nsec -= 1000000000;
+            ++starttime.tv_sec;
+          }
+        }
+        break;
+      case 'q':
+        // quit
+        cleanup();
+        exit(0);
+        break;
+      default:
+        break;
+    }
+  }
+  return NULL;
 }
 
 int main(int argc, char *argv[])
 {
   int c;
   static struct option long_options[] = {
-      {"restore", no_argument, 0, 'r'},
-      {"save", no_argument, 0, 's'}};
+    {"restore", no_argument, 0, 'r'},
+    {"save", no_argument, 0, 's'}};
   int option_index = 0;
 
   while ((c = getopt_long(argc, argv, "sr", long_options, &option_index)) != -1)
   {
     switch (c)
     {
-    case 'r':
-      rflag = 1;
-      restore_time();
-      break;
-    case 's':
-      sflag = 1;
-      break;
-    case '?':
-      break;
-    default:
-      exit(1);
+      case 'r':
+        rflag = 1;
+        restore_time();
+        break;
+      case 's':
+        sflag = 1;
+        break;
+      case '?':
+        break;
+      default:
+        exit(1);
     }
   }
   signal(SIGINT, sigint_handler);
+  pthread_t tid;
+  pthread_create(&tid, NULL, input_thread, NULL);
   HIDE_CURSOR;
   clock_gettime(CLOCK_MONOTONIC, &starttime);
   if (rflag)
@@ -188,18 +250,20 @@ int main(int argc, char *argv[])
       --starttime.tv_sec;
     }
   }
-  while (timing)
-  {
-    clock_gettime(CLOCK_MONOTONIC, &currenttime);
-    elapsedtime.tv_sec = currenttime.tv_sec - starttime.tv_sec;
-    elapsedtime.tv_nsec = currenttime.tv_nsec - starttime.tv_nsec;
-    if (elapsedtime.tv_nsec < 0)
+  while (1) {
+    while (timing)
     {
-      elapsedtime.tv_nsec += 1000000000;
-      --elapsedtime.tv_sec;
+      clock_gettime(CLOCK_MONOTONIC, &currenttime);
+      elapsedtime.tv_sec = currenttime.tv_sec - starttime.tv_sec;
+      elapsedtime.tv_nsec = currenttime.tv_nsec - starttime.tv_nsec;
+      if (elapsedtime.tv_nsec < 0)
+      {
+        elapsedtime.tv_nsec += 1000000000;
+        --elapsedtime.tv_sec;
+      }
+      print_time(stdout);
+      nanosleep(&sleepduration, NULL);
     }
-    print_time(stdout);
-    nanosleep(&sleepduration, NULL);
   }
   return 0;
 }
